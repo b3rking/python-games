3ba8b3dc2eab2993d31ca419105a8d98fdf12fff
b3rking <ishimweeschylebersy@gmail.com>
2021-05-24 10:32:06 +0200

    added fonction game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -24,3 +24,38 @@ def game():
 	computer = radom.randint(1, 3)
 	result = (player, computer)
 	return play_again()
+
+def move():
+	while True:
+		print
+		player = input("Rock - 1\nPaper - 2\nScissor - 3")
+
+		try:
+			player = int(player)
+			if player in (1,2,3):
+				return player
+		except ValueError:
+			pass
+		print("oops i didn't understand that. please enter 1, 2 or 3")
+
+def result(player, computer):
+
+	print("1...")
+	time.sleep(1)
+	print("2...")
+	time.sleep(1)
+	print("3!")
+	time.sleep(0.5)
+
+	print "computer threw {0}".format(names[computer])
+	global player_score, computer_score
+
+	if player == computer:
+		print "tie game!"
+	else:
+		if rules[player] == computer:
+			print "your victory has been assured"
+			player_score += 1
+		else:
+			print "the computer laughs as you realise you have been defeated"
+			computer_score += 1
